1a2e3b44c75999c8ba45c2fec372dc7a4ed460bd
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2019-04-24 16:24:24 +0300

    customize reponse

--- server/datedata/serializers.py
+++ server/datedata/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import DateData
+from rest_framework.response import Response
+
 
 class DateDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = DateData
         fields = ('pub_date','total_messages','total_customers')
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid(raise_exception=False):
+            return Response({"Fail": "blablal"})
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({"Success": "msb blablabla"})
--- server/datedata/views.py
+++ server/datedata/views.py
@@ -8,7 +8,6 @@ from .serializers import DateDataSerializer
 
 class DateDataView(ListAPIView):
     serializer_class = DateDataSerializer
-    
     def get_queryset(self):
         start_date_arr =  self.request.query_params.get('start_date',None).split('-')
         end_date_arr = self.request.query_params.get('end_date',None).split('-')
@@ -16,4 +15,19 @@ class DateDataView(ListAPIView):
         end_date = datetime(int(end_date_arr[0]),int(end_date_arr[1]),int(end_date_arr[2]))
         data = DateData.objects.filter(pub_date__range=(start_date,end_date))
         return data
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = DateDataSerializer(queryset, many=True)
+        totalMessagesArr = list(map(lambda x: x["total_messages"], serializer.data))
+        totalMessages = sum(totalMessagesArr)
+        totalCustomersArr = list(map(lambda x: x["total_customers"], serializer.data))
+        totalCustomers = sum(totalCustomersArr)
+        return Response({
+                "start_date": self.request.query_params.get('start_date',None),
+                "end_date": self.request.query_params.get('end_date',None),
+                "totalMessages": totalMessages,
+                "totalCustomers": totalCustomers,
+                "by_date": serializer.data
+            })
 
