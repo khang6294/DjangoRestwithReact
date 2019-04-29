d2b3c8d199bbbf988a84aa11adf5b4fc606720a1
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2019-04-29 22:09:24 +0300

    Customize search, filter, display and edit fields in admin page

--- server/datedata/admin.py
+++ server/datedata/admin.py
@@ -6,4 +6,12 @@ from .models import DateData
 class DateDataAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'total_customers', 'total_messages']
 
+    search_fields = ['pub_date','total_customers']
+
+    list_filter = ['total_customers','total_messages']
+
+    list_display = ['pub_date','total_customers','total_messages']
+
+    list_editable = ['total_customers']
+
 admin.site.register(DateData,DateDataAdmin)
